a89fb53262490150d2363bf6d3181e0874cdef77
Sanja Daic <sanja.daic86@gmail.com>
2026-01-14 10:50:09 +0100

    starting project

---
+++ src/data.js
@@ -0,0 +1,71 @@
+export const data = {
+  results: [
+    {
+      question: "Which of the following is NOT a venomous snake?",
+      correct_answer: "Reticulated python",
+      incorrect_answers: [
+        "Malayan Krait",
+        "Yellow bellied sea snake",
+        "Black mamba",
+      ],
+    },
+    {
+      question:
+        "Which of the following is true when alligators are behaving territorially?",
+      correct_answer: "They bellow while showing their tail and neck",
+      incorrect_answers: [
+        "Open their jaws while making a clicking noise",
+        "They run full force at the threat",
+        "Slap their tails on the ground",
+      ],
+    },
+    {
+      question:
+        "&quot;Decapods&quot; are an order of ten-footed crustaceans.  Which of these are NOT decapods?",
+      correct_answer: "Krill",
+      incorrect_answers: ["Lobsters", "Shrimp", "Crabs"],
+    },
+    {
+      question:
+        "The now extinct species &quot;Thylacine&quot; was native to where?",
+      correct_answer: "Tasmania, Australia",
+      incorrect_answers: [
+        "Baluchistan, Pakistan",
+        "Wallachia, Romania",
+        "Oregon, United States",
+      ],
+    },
+    {
+      question: "What is the fastest animal?",
+      correct_answer: "Peregrine Falcon",
+      incorrect_answers: ["Golden Eagle", "Cheetah", "Horsefly"],
+    },
+    {
+      question:
+        "What is the name for a male bee that comes from an unfertilized egg?",
+      correct_answer: "Drone",
+      incorrect_answers: ["Soldier", "Worker", "Male"],
+    },
+    {
+      question: "The dish Fugu, is made from what family of fish?",
+      correct_answer: "Pufferfish",
+      incorrect_answers: ["Bass", "Salmon", "Mackerel"],
+    },
+    {
+      question: "Cashmere is the wool from which kind of animal?",
+      correct_answer: "Goat",
+      incorrect_answers: ["Sheep", "Camel", "Llama"],
+    },
+    {
+      question:
+        "What is the name of the family that the domestic cat is a member of?",
+      correct_answer: "Felidae",
+      incorrect_answers: ["Felinae", "Felis", "Cat"],
+    },
+    {
+      question: "What is the collective noun for rats?",
+      correct_answer: "Mischief",
+      incorrect_answers: ["Pack", "Race", "Drift"],
+    },
+  ],
+};
