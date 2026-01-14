cb4f065151c6eb7793d6c4f6fbd1b8fea65e1eef
Sanja Daic <sanja.daic86@gmail.com>
2026-01-14 12:07:16 +0100

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="quizWrappepr">
+      <h1>Question</h1>
+      <div className="questions">
+        <div className="question">
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+            ipsa esse perferendis in molestias? Reprehenderit!dolor sit, amet
+            consectetur adipisicing elit. Temporibus ipsa esse perferendis in
+            molestias? Reprehenderit!
+          </span>
+        </div>
+      </div>
+
+      <div className="answers">
+        <div className="answer">
+          <span>A</span>
+          <span>Answer</span>
+        </div>
+        <div className="answer">
+          <span>B</span>
+          <span>Answer</span>
+        </div>
+        <div className="answer">
+          <span>C</span>
+          <span>Answer</span>
+        </div>
+        <div className="answer">
+          <span>D</span>
+          <span>Answer</span>
+        </div>
+      </div>
+      <div className="btnWrapper">
+        <button>Next Question</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
